f959c6d0ba3066e5f227aeec0a73a7cbb75b2145
vrushalitarte01 <tarte.v@northeastern.edu>
2022-11-28 13:13:32 -0800

    heroku?

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost:27017/tuiter');
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
