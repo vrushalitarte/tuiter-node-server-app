fd27ce4723cbe9f0f0fb12718befcf226bda3cfe
vrushalitarte01 <tarte.v@northeastern.edu>
2022-11-28 13:52:04 -0800

    update schema

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -3,5 +3,7 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    dislikes: Number,
+    disliked: Boolean,
 }, {collection: 'tuits'});
 export default schema;
